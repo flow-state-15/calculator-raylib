6bbbaf1306d8b4dcf1c8418af26bc3387c83d3da
Dan Purcell <purcellgroup88@gmail.com>
2024-04-17 17:06:42 -0400

    window init and focux logic

---
+++ hello.py
@@ -0,0 +1,7 @@
+def hello():
+  num = 15
+  print(f"well, hello there! vars: {num}")
+  return num
+
+
+hello()
--- main.c
+++ main.c
@@ -1,7 +1,68 @@
-#include <stdio.h>
 #include <raylib.h>
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+
+#define APP_NAME "Simple Calculator"
+
+static bool wasFocused = true;
+
+void sleep_program(int ms) {
+#ifdef _WIN32
+  Sleep(ms);
+
+#else
+  struct timespec ts;
+  ts.tv_sec = ms / 1000;
+  ts.tv_nsec = (ms % 1000) * 1000000;
+  nanosleep(&ts, NULL);
+#endif
+}
+
+void init_window() {
+  // init window
+  InitWindow(300, 700, APP_NAME);
+  SetTargetFPS(15);
+}
 
 int main(void) {
-  
+  // const char *py_cmd = "python ./hello.py";
+
+  // FILE *py_pipe = popen(py_cmd, "r");
+
+  // int py_fd = fileno(py_pipe);
+  // printf("FD: %d\n", py_fd);
+
+  // char buf[256];
+  // read(py_fd, buf, sizeof(buf));
+
+  // printf("Data: %s\n", buf);
+
+  // init window
+  init_window();
+
+  while (!WindowShouldClose()) {
+    bool isFocused = IsWindowFocused();
+
+    if (isFocused && !wasFocused) {
+      printf("Window focused\n");
+    } else if (!isFocused && wasFocused) {
+      printf("Window unfocused\n");
+    }
+
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    if (isFocused) {
+      DrawText("Hello, World!", 10, 10, 20, DARKGRAY);
+    } else {
+      sleep_program(20);
+    }
+    EndDrawing();
+
+    wasFocused = isFocused;
+  }
+
+  CloseWindow();
+
   return 0;
 }

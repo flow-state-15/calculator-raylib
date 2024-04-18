750bd3c3cfa7528ab0e3ff82c96633f818f678d4
9ziggy9 <davidarogers@protonmail.com>
2024-04-17 22:58:59 -0700

    preparing for merge conflict

--- main.c
+++ main.c
@@ -80,7 +80,6 @@ void init_button_geo(Button btns[NUMBER_OF_BUTTONS]) {
     int startX = 4;  // Edge padding
     int startY = 327 - 2 - 49;  // Bottom edge padding and one button height
     
-
     btns[0] = make_btn(startX, startY, 49, LIGHTGRAY, "C", true);
     btns[1] = make_btn(startX + colWidth, startY, 49, LIGHTGRAY, "=", true);
     btns[2] = make_btn(startX + (colWidth * 3), startY - rowHeight,
@@ -118,65 +117,38 @@ void init_button_geo(Button btns[NUMBER_OF_BUTTONS]) {
 #endif
 }
 
-/* void draw_button_grid(Button *buttons) { */
-/*     // Operator buttons on the right */
-/*     Vector2 divide_button = DrawSquare(startX + (colWidth * 3), startY - rowHeight, 49, LIGHTGRAY, "/"); */
-/*     Vector2 multiply_button = DrawSquare(startX + (colWidth * 3), startY - 2 * rowHeight, 49, LIGHTGRAY, "*"); */
-/*     Vector2 minus_button = DrawSquare(startX + (colWidth * 3), startY - 3 * rowHeight, 49, LIGHTGRAY, "-"); */
-/*     Vector2 plus_button = DrawSquare(startX + (colWidth * 3), startY - 4 * rowHeight, 49, LIGHTGRAY, "+"); */
-    
-/*     // Special function buttons */
-/*     Vector2 clear_button = DrawDSquare(startX, startY, 49, LIGHTGRAY, "C"); */
-/*     Vector2 equals_button = DrawDSquare(startX + colWidth, startY, 49, LIGHTGRAY, "="); */
-/*     Vector2 open_paren_button = DrawSquare(startX + colWidth, startY - rowHeight, 49, LIGHTGRAY, "("); */
-/*     Vector2 close_paren_button = DrawSquare(startX + (colWidth * 2), startY - rowHeight, 49, LIGHTGRAY, ")"); */
-
-/*     // Number buttons */
-/*     Vector2 zero_button = DrawSquare(startX + colWidth, startY - rowHeight, 49, LIGHTGRAY, "0"); */
-/*     Vector2 one_button = DrawSquare(startX, startY - 2 * rowHeight, 49, LIGHTGRAY, "1"); */
-/*     Vector2 two_button = DrawSquare(startX + colWidth, startY - 2 * rowHeight, 49, LIGHTGRAY, "2"); */
-/*     Vector2 three_button = DrawSquare(startX + 2 * colWidth, startY - 2 * rowHeight, 49, LIGHTGRAY, "3"); */
-/*     Vector2 four_button = DrawSquare(startX, startY - 3 * rowHeight, 49, LIGHTGRAY, "4"); */
-/*     Vector2 five_button = DrawSquare(startX + colWidth, startY - 3 * rowHeight, 49, LIGHTGRAY, "5"); */
-/*     Vector2 six_button = DrawSquare(startX + 2 * colWidth, startY - 3 * rowHeight, 49, LIGHTGRAY, "6"); */
-/*     Vector2 seven_button = DrawSquare(startX, startY - 4 * rowHeight, 49, LIGHTGRAY, "7"); */
-/*     Vector2 eight_button = DrawSquare(startX + colWidth, startY - 4 * rowHeight, 49, LIGHTGRAY, "8"); */
-/*     Vector2 nine_button = DrawSquare(startX + (colWidth * 2), startY - 4 * rowHeight, 49, LIGHTGRAY, "9"); */
-/* } */
-
-
 int main(void) {
   Button buttons[NUMBER_OF_BUTTONS];
   init_button_geo(buttons);
   print_buttons(buttons);
 
-  /* init_window(); */
+  init_window();
 
-  /* while (!WindowShouldClose()) { */
-  /*   bool isFocused = IsWindowFocused(); */
-  /*   draw_button_grid(&buttons); */
+  while (!WindowShouldClose()) {
+    bool isFocused = IsWindowFocused();
+    draw_button_grid(&buttons);
 
-  /*   if (isFocused && !wasFocused) { */
-  /*     printf("Window focused\n"); */
-  /*   } else if (!isFocused && wasFocused) { */
-  /*     printf("Window unfocused\n"); */
-  /*   } */
+    if (isFocused && !wasFocused) {
+      printf("Window focused\n");
+    } else if (!isFocused && wasFocused) {
+      printf("Window unfocused\n");
+    }
 
-  /*   BeginDrawing(); */
-  /*   ClearBackground(RAYWHITE); */
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
 
-  /*   if (isFocused) { */
-  /*     DrawText("Hello, World!", 10, 10, 20, DARKGRAY); */
+    if (isFocused) {
+      DrawText("Hello, World!", 10, 10, 20, DARKGRAY);
       
-  /*   } else { */
-  /*     sleep_program(20); */
-  /*   } */
+    } else {
+      sleep_program(20);
+    }
 
-  /*   EndDrawing(); */
-  /*   wasFocused = isFocused; */
-  /* } */
+    EndDrawing();
+    wasFocused = isFocused;
+  }
 
-  /* CloseWindow(); */
+  CloseWindow();
 
   return 0;
 }
